184024060c2d905a7c804d1322f42bb1d00bdbd5
pamela-trindade <89667148+pamela-trindade@users.noreply.github.com>
2025-08-28 16:13:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/teste1")
+@app.get("/funcaoteste")
 async def funcaoteste():
-    return {"teste": "deu bão"}
+    return {"eai galera"}
+
